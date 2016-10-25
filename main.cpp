0798cbc3b8d6619158ca2659485f366f4781d65a
gurusharan2 <gurusharan2@gmail.com>
2016-10-26 00:02:22 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,9 @@
-//
-//  main.cpp
-//  Linker List 24-10-16
-//
-//  Created by Dinker on 10/24/16.
-//  Copyright © 2016 Dinker. All rights reserved.
-//
-
 #include <iostream>
+
 using namespace std;
-struct node{
-    node* link;
-    int data;
-};
 
-void display(node* temp){
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->link;
-    }
-}
-
-int main() {
-    node *head=NULL;
-    display(head);
+int main()
+{
+    cout << "Hello world!" << endl;
     return 0;
 }
